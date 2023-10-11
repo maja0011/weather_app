ce54432e56d9830f94d732eeb9e3456295775dd3
maja0011 <maja0011@algonquinlive.com>
2023-10-11 03:17:05 -0400

    Completed the functionality of weather app

--- src/components/WeatherApp.jsx
+++ src/components/WeatherApp.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import './WeatherApp.css';
+import { useEffect, useState } from "react";
 
 import search_icon from './assets/search.png';
 import clear_icon from './assets/clear.png';
@@ -10,38 +11,96 @@ import snow_icon from './assets/snow.png';
 import wind_icon from './assets/wind.png';
 import humidity_icon from './assets/humidity.png';
 
-function WeatherApp () {
+
+const API_KEY = '284caa3b1b58571adbab78df74ab7e7e';
+
+function WeatherApp() {
+    const [dataRecieved, setDataRecieved] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('ottawa');
+    const [weatherIcon, setWeatherIcon] = useState(cloud_icon);
+    const API_URL = `https://api.openweathermap.org/data/2.5/weather?q=${searchTerm}&units=Metric&appid=${API_KEY}`;
+
+    const searchWeather = async () => {
+        const response = await fetch(API_URL);
+        const data = await response.json();
+        if(data.cod === "404"){
+            alert(`The City ${searchTerm} is not a city registered on the open weather database, Please try a different city`);
+        }
+          setDataRecieved(data);
+
+          switch(dataRecieved?.weather?.[0]?.icon){
+            case "01d":
+            case "01n":
+                setWeatherIcon(clear_icon);
+                break;
+            case "09d":
+            case "09n":
+                setWeatherIcon(drizzle_icon);
+                break;
+            case "10d":
+            case "10n":
+                setWeatherIcon(rain_icon);
+                break;
+            case "13d":
+            case "13n":
+                setWeatherIcon(snow_icon);
+                break;
+
+            default :
+            setWeatherIcon(cloud_icon);
+          }
+
+
+
+    }
+
+    useEffect(() => {
+        searchWeather();
+    }, []);
+
+
+
+
     return (
+        
+
         <div className="container">
             <div className="top-bar">
-                <input type="text" className="cityInput"placeholder="City"/>
-                <div className="search-icon">
-                    <img src={search_icon} alt="the search icon"/>
+                <input type="text" className="cityInput" placeholder="City" onChange={(e) => setSearchTerm(e.target.value)} />
+                <div className="search-icon" onClick={() => searchWeather()}>
+                    <img src={search_icon} alt="the search icon" />
                 </div>
             </div>
 
+            
+            
+
+            
+
+
             <div className="weather-image">
-                <img src={cloud_icon} alt= 'a cloud icon' />
+                <img src={weatherIcon} alt='a weather icon' />
             </div>
-            <div className="weather-temp">24°C</div>
-            <div className="weather-location">Ottawa</div>
+            <div className="weather-temp">{Math.floor(dataRecieved?.main?.temp)}°C</div>
+            <div className="weather-location">{dataRecieved?.name}</div>
+            <div className="weather-location-feels">Feels Like {Math.floor(dataRecieved?.main?.feels_like)}°C </div>
             <div className="data-container">
 
-        <div className="element">
-            <img src={humidity_icon} alt="" className="icon" />
-            <div className="data">
-                <div className="humidity-percent">64%</div>
-                <div className="text">Humidity</div>
-            </div>
-            </div>
+                <div className="element">
+                    <img src={humidity_icon} alt="" className="icon" />
+                    <div className="data">
+                        <div className="humidity-percent">{dataRecieved?.main?.humidity}%</div>
+                        <div className="text">Humidity</div>
+                    </div>
+                </div>
 
-            <div className="element">
-            <img src={wind_icon} alt="" className="icon" />
-            <div className="data">
-                <div className="humidity-percent">18 km/h</div>
-                <div className="text">Wind Speed</div>
-            </div>
-            </div>
+                <div className="element">
+                    <img src={wind_icon} alt="" className="icon" />
+                    <div className="data">
+                        <div className="humidity-percent">{dataRecieved?.wind?.speed} km/h</div>
+                        <div className="text">Wind Speed</div>
+                    </div>
+                </div>
 
             </div>
 
